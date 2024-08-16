ca4c6b8ee51bd19cb33d0d9bc9e280d79d928a97
AnmolNisar <anmolnisar24@gmail.com>
2024-08-17 00:47:04 +0500

    Completed data streaming implementation and updated graph component

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,11 +1,8 @@
 export interface Order {
-  price: Number,
-  size: Number,
+  price: number,
+  size: number,
 }
-/**
- * The datafeed server returns an array of ServerRespond with 2 stocks.
- * We do not have to manipulate the ServerRespond for the purpose of this task.
- */
+
 export interface ServerRespond {
   stock: string,
   top_bid: Order,
@@ -14,24 +11,28 @@ export interface ServerRespond {
 }
 
 class DataStreamer {
-  // The url where datafeed server is listening
+  // The URL where the data feed server is listening
   static API_URL: string = 'http://localhost:8080/query?id=1';
 
   /**
-   * Send request to the datafeed server and executes callback function on success
+   * Send request to the data feed server and executes callback function on success
    * @param callback callback function that takes JSON object as its argument
    */
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+    request.open('GET', DataStreamer.API_URL, true); // Change to asynchronous
 
     request.onload = () => {
       if (request.status === 200) {
         callback(JSON.parse(request.responseText));
       } else {
-        alert ('Request failed');
+        console.error('Request failed with status:', request.status);
       }
-    }
+    };
+
+    request.onerror = () => {
+      console.error('Request error');
+    };
 
     request.send();
   }
